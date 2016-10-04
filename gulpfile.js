9f12431143a3f51a6ea971e48a2c33abe48e4c13
Steve Rubinstein <slrubinstein@gmail.com>
2016-10-03 21:49:19 -0400

    create basic project structure

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const watch = require('gulp-watch');
+
+gulp.task('default', ['browserify', 'copy-html', 'watch']);
+
+gulp.task('browserify', function() {
+  return browserify('./source/app.js')
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/'));
+});
+
+const watchers = [
+  'source/**/*.js',
+  'source/assets/*.json'
+];
+
+gulp.task('watch', function() {
+  return watch(watchers, function () {
+    gulp.start('browserify');
+  });
+});
+
+gulp.task('copy-html', function() {
+  gulp.src('./source/index.html')
+  .pipe(gulp.dest('./build'));
+});
